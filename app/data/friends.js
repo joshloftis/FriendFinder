c3da3db405deefb8cc105a36bee0b7273a12cb57
Josh Loftis <jshlfts32@gmail.com>
2017-11-08 22:14:36 -0500

    Working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 const friends = [
   {
     "name": "Anokhee",
-    "photo": "",
+    "photo": "images/anokhee.jpg",
     "scores": [1, 4, 5, 4, 3, 4, 3, 5, 1, 5]
   },
   {
     "name": "Josh",
-    "photo": "",
+    "photo": "images/josh.jpg",
     "scores": [3, 4, 2, 5, 3, 2, 3, 3, 5, 1]
   },
   {
     "name": "Sardine",
-    "photo": "",
+    "photo": "images/sardine.jpg",
     "scores": [2, 3, 1, 4, 1, 3, 1, 5, 5, 4]
   },
   {
     "name": "Daisy",
-    "photo": "",
+    "photo": "images/daisy.jpg",
     "scores": [5, 2, 4, 3, 2, 5, 3, 1, 4, 5]
   }
 ];
--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/app/public"));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'app/public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
